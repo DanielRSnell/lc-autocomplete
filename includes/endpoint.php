32d9be5db0c2827f10d20d3726883537166c7d0e
Artisan <daniel@domartisan.com>
2023-12-04 11:19:11 -0500

    Detailed comments about REST and the handler.

--- includes/endpoint.php
+++ includes/endpoint.php
@@ -1,18 +1,33 @@
-<?php 
+<?php
 
-
+/**
+ * Handles requests for the completions endpoint.
+ *
+ * This function is designed to handle the logic for the REST API endpoint that provides
+ * autocomplete suggestions. It uses a filter to modify and retrieve the completions array,
+ * then sends it as a JSON response.
+ *
+ * @return void Outputs the JSON response and exits.
+ */
 function handle_completions_endpoint() {
     // Apply a filter to get the completions array. 
+    // Developers can modify this array by hooking to the 'lc_modify_completions' filter.
     $completions = apply_filters('lc_modify_completions', []);
 
-    // Return the completions array as a JSON response.
+    // Send the completions array as a JSON response.
     wp_send_json($completions);
 }
 
-// Register the REST API route.
+/**
+ * Registers the REST API route for completions.
+ *
+ * This function adds a custom REST API route to WordPress. The route is used to retrieve
+ * autocomplete suggestions for the Live Canvas editor. It defines a GET method endpoint 
+ * at '/livecanvas/v1/completions'.
+ */
 add_action('rest_api_init', function () {
     register_rest_route('livecanvas/v1', '/completions', [
-        'methods' => 'GET',
-        'callback' => 'handle_completions_endpoint',
+        'methods' => 'GET',  // Define the HTTP method as GET.
+        'callback' => 'handle_completions_endpoint', // Specify the callback function for this endpoint.
     ]);
 });
